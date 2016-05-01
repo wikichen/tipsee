d55dbd22c26f3f23bc8e65b4c3a91878ea31f5c9
Jonathan E. Chen <code@chen.io>
2016-05-01 02:30:27 -0700

    Ship v1 with minimal love

--- tipsee/ViewController.swift
+++ tipsee/ViewController.swift
@@ -9,17 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(sender: AnyObject) {
+        let tipOptions = [0.15, 0.2, 0.25]
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipOptions[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
 }
+
 
